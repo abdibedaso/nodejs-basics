390ce66e587218c8c061aab889a2dec84605fc4b
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-22 21:14:19 -0500

    Imp custom module app

---
+++ 03-creatingAModule/app.js
@@ -0,0 +1,5 @@
+// Modules
+// Node implements the CommonJS module specification. 
+// ...This allows the programmer to include additional libraries (.js code files) 
+// ...into their code using the require() function, 
+// ...which returns the module.exports object defined in the specified .js file.
